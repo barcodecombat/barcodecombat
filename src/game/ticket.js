0efc515f5c6f27c782a5a267822396d642a614b0
thyshimrod <thyshimrod@gmail.com>
2018-10-08 08:24:35 +0200

    On opening chests gain a new ticket to have a scan

---
+++ src/game/ticket.js
@@ -0,0 +1,15 @@
+'use strict';
+var barcode = barcode || {};
+
+barcode.Ticket = function(){
+  this.legend = 0;
+  this.rare = 0;
+  this.uncommon = 0;
+};
+
+
+barcode.Ticket.prototype = {
+  init : function(){
+    
+  }
+}
